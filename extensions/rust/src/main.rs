1aa1081e958ae0e09a63dd463cfdf7cfc0109198
Harald Schneider <h_schneider@marketmix.com>
2024-02-08 09:29:33 +0100

    Update

--- extensions/rust/src/main.rs
+++ extensions/rust/src/main.rs
@@ -34,10 +34,14 @@ fn process_app_event(ext: &mut neutralino::Extension, d: &mut serde_json::Value)
         }
 
         // Experimental:
-        // This starts a long running background task, which reports
+        // This starts a long-running background-task, which reports
         // its progress to stdout.
-        // Using a scoped thread which borrows the ext object will block
-        // until finished. So using ext.send_message() won't work.
+        // Using a scoped thread here instead won't work:
+        // In this case the thread borrows the ext object, which will block
+        // until finished due to its lifetime. So using ext.send_message()
+        // to report progress won't be possible.
+        // Reporting progress to the frontend will require some other IPC-channel
+        // or a polling solution.
         //
         if data["function"].as_str().unwrap() == "longRun" {
             long_run(ext);
@@ -46,6 +50,9 @@ fn process_app_event(ext: &mut neutralino::Extension, d: &mut serde_json::Value)
 }
 
 fn long_run(ext: &mut neutralino::Extension) {
+    //
+    // Spawn a background-task
+
     ext.send_message("pingResult", "Long running task started.");
 
     std::thread::spawn(|| {
